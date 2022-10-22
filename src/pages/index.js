67a4d76480f205ef86d9e62e786f2ca738ac188b
vickov <vickovitasovic@gmail.com>
2022-10-22 10:20:31 +0200

    Test mobile responsitivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,8 +41,8 @@ const responsive = {
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1 // optional, default to 1.
+    items: 3,
+    slidesToSlide: 2 // optional, default to 1.
   }
 };
 
